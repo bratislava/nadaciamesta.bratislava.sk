cc9b0207f6d6cd0d955ffdd3452dff27741e3e51
Martin Pinter <martin.pinter@vacuumlabs.com>
2022-10-06 10:43:57 +0200

    Comment out seed

--- strapi/config/functions/seeders/simpleSeed.js
+++ strapi/config/functions/seeders/simpleSeed.js
@@ -1,44 +1,40 @@
-'use strict';
+"use strict";
 
-const DATA = require('./data/simpleSeedData');
+// const DATA = require('./data/simpleSeedData');
 
 /**
  * Just a simple seeding function
  */
 
 module.exports = () => {
-  const collectionNames = Object.keys(DATA);
-
-  collectionNames.forEach((collectionName) => {
-    const data = DATA[collectionName];
-
-    if (strapi.services[collectionName] == undefined) {
-      console.error(
-        `SEEDING ERROR: service '${collectionName}' does not exist\n`,
-        'AVAILABLE SERVICES: ',
-        Object.keys(strapi.services)
-      );
-      return;
-    }
-
-    //collection-type
-    if (Array.isArray(data)) {
-      data.forEach(async (item) => {
-        const foundItems = await strapi.services[collectionName].find(item);
-
-        // if item not found, then seed it
-        if (foundItems.length === 0) {
-          strapi.services[collectionName].create(item);
-        }
-      });
-    }
-    //single-type
-    else if (typeof data === 'object') {
-      strapi.services[collectionName].createOrUpdate(data);
-    } else {
-      console.error(
-        `SEEDING ERROR: '${collectionName}' is is neither an object nor an array`
-      );
-    }
-  });
+  // const collectionNames = Object.keys(DATA);
+  // collectionNames.forEach((collectionName) => {
+  //   const data = DATA[collectionName];
+  //   if (strapi.services[collectionName] == undefined) {
+  //     console.error(
+  //       `SEEDING ERROR: service '${collectionName}' does not exist\n`,
+  //       'AVAILABLE SERVICES: ',
+  //       Object.keys(strapi.services)
+  //     );
+  //     return;
+  //   }
+  //   //collection-type
+  //   if (Array.isArray(data)) {
+  //     data.forEach(async (item) => {
+  //       const foundItems = await strapi.services[collectionName].find(item);
+  //       // if item not found, then seed it
+  //       if (foundItems.length === 0) {
+  //         strapi.services[collectionName].create(item);
+  //       }
+  //     });
+  //   }
+  //   //single-type
+  //   else if (typeof data === 'object') {
+  //     strapi.services[collectionName].createOrUpdate(data);
+  //   } else {
+  //     console.error(
+  //       `SEEDING ERROR: '${collectionName}' is is neither an object nor an array`
+  //     );
+  //   }
+  // });
 };
